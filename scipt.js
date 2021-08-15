ce06178a8299f974cff689087f4bfe35bc76bb8a
rishbitsbytes <88099761+rishbitsbytes@users.noreply.github.com>
2021-08-15 23:31:18 +0530

    initial commit

---
+++ scipt.js
@@ -0,0 +1,34 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkNumberButton = document.querySelector("#check-number");
+const outputBox = document.querySelector("#output-box");
+
+
+checkNumberButton.addEventListener('click',checkBirthDateIsLucky)
+
+
+function compareValues(sum,luckyNumber){
+  if(sum%luckyNumber===0){
+    outputBox.innerText="Your birthday is lucky 🥳";
+  }else {
+    outputBox.innerText="Your birthday is not lucky ☹️ Don't worry this is not the end of the world!";
+  }
+}
+
+function checkBirthDateIsLucky(){
+  const dob =dateOfBirth.value;
+  const sum = calculateSum(dob);
+  if(sum&&dob)
+  compareValues(sum,luckyNumber.value)
+  else 
+  outputBox.innerText="Please enter both the fields 😡";
+}
+
+function calculateSum(dob) {
+  dob= dob.replaceAll("-","");
+  let sum =0;
+  for(let index=0;index<dob.length;index++){
+    sum = sum + Number(dob.charAt(index));
+  }
+  return sum;
+}
